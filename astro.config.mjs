bcd52d390738115478d07fa13dd8cb9889b95daf
luca <meltflake@gmail.com>
2026-03-15 10:05:18 +0800

    fix: update site URL to meltflake.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://meltflake.github.io',
+  site: 'https://meltflake.com',
   base: '/creatorcut',
   output: 'static',
   vite: {
